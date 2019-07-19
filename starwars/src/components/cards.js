911d0f4a49fd96c49a4eaa5039dac7e410984b2b
Christopher Pupo <chrispupo@Christophers-iMac.local>
2019-07-19 16:35:44 -0400

    created the card component

--- starwars/src/components/cards.js
+++ starwars/src/components/cards.js
@@ -1,1 +1,15 @@
-import './cards.css';
+import React from 'react';
+import './cards.scss';
+
+export default function Cards(props){
+
+    return(
+        <div className='card-container'>
+            <h2 className= 'character-name'>{ props.charName }</h2>
+                <div>
+                    <p className= 'character-gender'>{ props.charGender }</p>
+                    <p className= 'birth-year'>{ props.charYear }</p>
+                </div>
+        </div>
+    )
+}
